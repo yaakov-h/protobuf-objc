b12f9b6e1e44e92df475f4a8a5d98cffbc48b286
Yaakov <pubgit@codingrange.com>
2012-12-09 13:47:08 +1100

    Fixed not being able to generate a protobuf with a field named "result"

--- src/compiler/objc_message.cc
+++ src/compiler/objc_message.cc
@@ -837,13 +837,13 @@ namespace google { namespace protobuf { namespace compiler { namespace objective
   void MessageGenerator::GenerateBuilderSource(io::Printer* printer) {
     printer->Print(
       "@interface $classname$_Builder()\n"
-      "@property (retain) $classname$* result;\n"
+      "@property (retain) $classname$* _builderResult;\n"
       "@end\n"
       "\n"
       "@implementation $classname$_Builder\n"
-      "@synthesize result;\n"
+      "@synthesize _builderResult;\n"
       "- (void) dealloc {\n"
-      "  self.result = nil;\n"
+      "  self._builderResult = nil;\n"
       "  [super dealloc];\n"
       "}\n",
       "classname", ClassName(descriptor_));
@@ -851,7 +851,7 @@ namespace google { namespace protobuf { namespace compiler { namespace objective
     printer->Print(
       "- (id) init {\n"
       "  if ((self = [super init])) {\n"
-      "    self.result = [[[$classname$ alloc] init] autorelease];\n"
+      "    self._builderResult = [[[$classname$ alloc] init] autorelease];\n"
       "  }\n"
       "  return self;\n"
       "}\n",
@@ -872,22 +872,22 @@ namespace google { namespace protobuf { namespace compiler { namespace objective
     if (descriptor_->extension_range_count() > 0) {
       printer->Print(
       "- (PBExtendableMessage*) internalGetResult {\n"
-      "  return result;\n"
+      "  return _builderResult;\n"
       "}\n");
     } else {
       printer->Print(
       "- (PBGeneratedMessage*) internalGetResult {\n"
-      "  return result;\n"
+      "  return _builderResult;\n"
       "}\n");
     }
 
     printer->Print(
       "- ($classname$_Builder*) clear {\n"
-      "  self.result = [[[$classname$ alloc] init] autorelease];\n"
+      "  _builderResult = [[[$classname$ alloc] init] autorelease];\n"
       "  return self;\n"
       "}\n"
       "- ($classname$_Builder*) clone {\n"
-      "  return [$classname$ builderWithPrototype:result];\n"
+      "  return [$classname$ builderWithPrototype:_builderResult];\n"
       "}\n"
       "- ($classname$*) defaultInstance {\n"
       "  return [$classname$ defaultInstance];\n"
@@ -909,8 +909,8 @@ namespace google { namespace protobuf { namespace compiler { namespace objective
 
     printer->Outdent();
     printer->Print(
-      "  $classname$* returnMe = [[result retain] autorelease];\n"
-      "  self.result = nil;\n"
+      "  $classname$* returnMe = [[_builderResult retain] autorelease];\n"
+      "  self._builderResult = nil;\n"
       "  return returnMe;\n"
       "}\n",
       "classname", ClassName(descriptor_));
